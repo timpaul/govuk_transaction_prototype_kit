af69406ae8424a01945710f8dee55a4286a88a81
Tim Paul <tim.paul@digital.cabinet-office.gov.uk>
2015-09-18 13:57:47 +0100

    Make screens data-driven

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,72 @@
 module.exports = {
   bind : function (app) {
 
-    app.get('/', function (req, res) {
-      res.render('index');
-    });
+  	var fs = require('fs');
 
-    app.get('/examples/template-data', function (req, res) {
-      res.render('examples/template-data', { 'name' : 'Foo' });
-    });
+	var services =  {
+  		services:[
+	    	{ url: "property-possession", name: "Repossess a property" },
+	    	{ url: "renew-passport", name: "Renew your passport"  },
+	    	{ url: "apprenticeships", name: "Apply for an apprenticeship"  },
+	    	{ url: "prison-visits", name: "Visit someone in prison"  },
+	    	{ url: "registered-traveller", name: "Apply to be a registered traveller" },
+	    	{ url: "redundancy-payments", name: "Claim redundancy payments"  },
+	    	{ url: "vehicle-operators", name: "Apply for a vehicle operator’s licence" },
+	    	{ url: "childminder", name: "Become a childminder" }
+		]
+	}
 
-    // add your routes here
+	var getServiceData = function(service, callback){
+    	var data = JSON.parse(fs.readFileSync('app/data/' + service + '.json', 'utf8'));
+    	return callback(null, data);
+	}
+
+
+
+
+		// Serve index page
+	    app.get('/', function (req, res) {
+	      res.render('index', services);
+	    });
+
+
+	    // Serve check pages
+	    app.get("/service/:service/check", function (req, res) {
+	      
+	      var service = req.params.service;
+
+	      getServiceData(service, function(error, data){
+
+	      	data.verb = "Check";
+	      	data.isCheck = true;
+
+	      	return res.render('examples/summary', data);
+	      })
+
+	    });
+
+	   	// Serve overview pages
+	    app.get("/service/:service/overview", function (req, res) {
+	      
+	      var service = req.params.service;
+
+	      getServiceData(service, function(error, data){
+
+	      	data.verb = "Complete";
+	      	data.isOverview = true;
+
+	      	return res.render('examples/summary', data);
+	      })
+
+	    });
+
+
+
+
+
+
+
+
 
   }
 };
